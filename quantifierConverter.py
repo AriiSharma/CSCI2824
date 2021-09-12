7fff399d230d43b7fd4d4958ed295a59e4f36984
Araju Sharma <89475631+AriiSharma@users.noreply.github.com>
2021-09-12 17:58:00 -0600

    Update quantifierConverter.py

--- quantifierConverter.py
+++ quantifierConverter.py
@@ -1,15 +1,12 @@
-#creating class name QuantifierConverter
 class QuantifierConverter(object):
-    #function name quantifierConverter with s as a parameter
+
     def quantifierConverter(self,s):
-        #using replace function to replace "\u2200" with "for every" in string s
+
         s = s.replace("\u2200", "for every")
-        #using replace function to replace "\u2203" with "there exists" in string s
         s = s.replace("\u2203","there exists")
-        #returning s
+
         return s
-#creating instance name obj of class QuantifierConverter to call function quantifierConverter
-obj = QuantifierConverter()
-#calling quantifierConverter function using instance 
-#passing string "\u2200 beautiful day there is, \u2203 some family time." as parameter
-print(instance.quantifierConverter("\u2200 beautiful day there is, \u2203 some family time."))
+
+    obj = QuantifierConverter()
+
+print(quantifierConverter("\u2200 beautiful day there is, \u2203 some family time."))
